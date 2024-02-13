939ec5ef37e0c00ee72a4348f0f247755b50934c
Vibha T R <126006781+Vibhatravi@users.noreply.github.com>
2024-02-13 18:29:02 +0530

    Add files via upload

---
+++ Program6/src/main/java/com/example/Program6/Program6Application.java
@@ -0,0 +1,13 @@
+package com.example.Program6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Program6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Program6Application.class, args);
+	}
+
+}
